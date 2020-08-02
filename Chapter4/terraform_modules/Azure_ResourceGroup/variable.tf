e032a56cd5e1eb5a1756d30d900cb590151e637d
Nishant Singh <nissingh@nissingh-mn1.linkedin.biz>
2020-08-02 22:30:04 +0530

    Added variable file

---
+++ Chapter4/terraform_modules/Azure_ResourceGroup/variable.tf
@@ -0,0 +1,15 @@
+variable "resource-grp-name" {
+  default = "test"
+}
+
+variable "azure-dc" {
+  default = "eastus"
+}
+
+variable "env" {
+  default = "stg"
+}
+
+variable "type-of-cluster"{
+  default = "test"
+}

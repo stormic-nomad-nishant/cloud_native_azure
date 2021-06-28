c4dda847ae29c8f2bb443d930d603a2efe662f3b
Matt Franz <mdfranz@gmail.com>
2021-06-28 18:25:48 -0400

    RabbitMQ Terraform Example

---
+++ Chapter9/rabbitmq/terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    rabbitmq = {
+      source = "cyrilgdn/rabbitmq"
+      version = "1.5.1"
+    }
+  }
+}
+
+provider "rabbitmq" {
+  endpoint = "http://192.168.2.31:15672"
+  username = "guest"
+  password = "guest"
+}
+
+# Create a virtual host
+resource "rabbitmq_vhost" "tfvhost" {
+  name = "tfvhost"
+}

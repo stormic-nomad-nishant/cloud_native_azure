a879797e91e7536a8df8eab2f031b508d4eb2c8b
Matt Franz <mdfranz@gmail.com>
2021-07-03 11:09:48 -0400

    Sending messages

--- Chapter9/azure/eventhub/golang/main.go
+++ Chapter9/azure/eventhub/golang/main.go
@@ -3,11 +3,13 @@ package main
 import (
   "os"
   "fmt"
+  "context"
 
 	eventhub "github.com/Azure/azure-event-hubs-go"
 )
 
 func main () {
+  ctx := context.Background()
   hub, err := eventhub.NewHubFromConnectionString(os.Getenv("EVENT_HUBS_NAMESPACE_CONNECTION_STRING"))
 
   if err !=  nil {
@@ -15,6 +17,9 @@ func main () {
   }
 
   fmt.Println(hub)
+  hub.Send(ctx,eventhub.NewEventFromString("hello Azure!"))
+  hub.Send(ctx,eventhub.NewEventFromString("hello Azure!"))
+  hub.Send(ctx,eventhub.NewEventFromString("hello Azure!"))
 
 }
 

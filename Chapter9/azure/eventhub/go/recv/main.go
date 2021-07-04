4cb93e375169273d2f1640640fdeaae63523ca37
Matt Franz <mdfranz@gmail.com>
2021-07-03 22:44:57 -0400

    Spaces are better thatn tabs

--- Chapter9/azure/eventhub/go/recv/main.go
+++ Chapter9/azure/eventhub/go/recv/main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-	"context"
-	"os"
+  "context"
+  "os"
   "os/signal"
   "fmt"
 
-	eventhub "github.com/Azure/azure-event-hubs-go"
+  eventhub "github.com/Azure/azure-event-hubs-go"
 )
 
 func main() {
-	ctx := context.Background()
-	hub, err := eventhub.NewHubFromConnectionString(os.Getenv("EVENTHUB_CONNECTION_STRING"))
+  ctx := context.Background()
+  hub, err := eventhub.NewHubFromConnectionString(os.Getenv("EVENTHUB_CONNECTION_STRING"))
 
   handler := func(c context.Context, event *eventhub.Event) error {
-		fmt.Println(string(event.Data))
-		return nil
-	}
+  fmt.Println(string(event.Data))
+    return nil
+  }
 
-	runtimeInfo, err := hub.GetRuntimeInformation(ctx)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+  runtimeInfo, err := hub.GetRuntimeInformation(ctx)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
-	for _, partitionID := range runtimeInfo.PartitionIDs {
+  for _, partitionID := range runtimeInfo.PartitionIDs {
 
-		listenerHandle, err := hub.Receive(ctx, partitionID, handler)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+    listenerHandle, err := hub.Receive(ctx, partitionID, handler)
+    if err != nil {
+      fmt.Println(err)
+    return
+  }
     fmt.Println(listenerHandle)
   }
 
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt, os.Kill)
-	<-signalChan
+  signalChan := make(chan os.Signal, 1)
+  signal.Notify(signalChan, os.Interrupt, os.Kill)
+  <-signalChan
 
-	err = hub.Close(context.Background())
-	if err != nil {
-		fmt.Println(err)
-	}
+  err = hub.Close(context.Background())
+  if err != nil {
+    fmt.Println(err)
+}
 }

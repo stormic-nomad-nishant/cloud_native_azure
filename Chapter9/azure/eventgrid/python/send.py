51a1bd68b18a74b4ed4dd5819b0d349686417b67
Matt Franz <mdfranz@gmail.com>
2021-07-05 12:31:48 -0400

    Python eventgrid client

---
+++ Chapter9/azure/eventgrid/python/send.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Adapted from  https://pypi.org/project/azure-eventgrid/#send-an-event-grid-event
+
+import os,sys
+from azure.core.credentials import AzureKeyCredential
+from azure.eventgrid import EventGridPublisherClient, EventGridEvent
+
+
+event = EventGridEvent(
+    data={"team": "azure-sdk"},
+    subject="Door1",
+    event_type="Azure.Sdk.Demo",
+    data_version="2.0"
+)
+
+try:
+    credential = AzureKeyCredential(os.environ['AZ_EVENTGRID_ACCESS_KEY'])
+    client = EventGridPublisherClient(sys.argv[1],credential)
+    client.send(event)
+
+except Exception as e:
+    print(e)
+
+
